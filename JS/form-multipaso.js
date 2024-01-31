640df4444cce805881d8cfcd68eca4ee967f8f03
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-01-31 19:20:11 -0300

    Se creó una segunda función para ocultar la segunda vista y mostrar la tercera al hacer click en el boton

--- JS/form-multipaso.js
+++ JS/form-multipaso.js
@@ -4,3 +4,10 @@ function primeraVista() {
   ocultarPrimeraVista.style.display = "none";
   mostrarSegundaVista.style.display = "flex";
 }
+
+function segundaVista() {
+  const ocultarSegundaVista = document.getElementById("segunda-vista");
+  const mostrarTerceraVista = document.getElementById("tercera-vista");
+  ocultarSegundaVista.style.display = "none";
+  mostrarTerceraVista.style.display = "flex";
+}
